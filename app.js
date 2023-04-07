16c6c2249eb6f9bfc3bcbb9131edef2b4e4b66b3
Mina Borhani <92939258+MinaBorhani@users.noreply.github.com>
2023-04-07 11:11:04 -0700

    new

--- app.js
+++ app.js
@@ -1,8 +1,21 @@
-let search =  document.getElementById("search-books");
-let bookList = document.getElementById("book-list");
+// let search =  document.getElementById("search-books");
+// let bookList = document.getElementById("book-list");
 
-let title =document.getElementsByClassName("title");
+// let title =document.getElementsByClassName("title");
 
-Array.from(title).forEach (function(title){
-    console.log(title);
-});
+// Array.from(title).forEach (function(title){
+//     console.log(title);
+// });
+
+
+
+// --remove Li tages ...
+let btns = document.querySelectorAll("#book-list .delete");
+
+Array.from(btns).forEach (function (bt){
+    bt.addEventListener("click" , function(e){
+        // console.log(e.target);
+        let targetEvent = e.target.parentElement;
+        targetEvent.parentNode.removeChild(targetEvent);
+    })
+})
